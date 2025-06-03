081decc8e2f1c1588b7293f0b33bd65ede867c26
Sander Blomvågnes <sander@blomvag.no>
2025-06-03 10:41:55 +0200

    ci: ignore default station vars as they should always exist

--- variables.tf
+++ variables.tf
@@ -19,6 +19,7 @@ variable "tags" {
   default     = {}
 }
 
+# tflint-ignore: terraform_unused_declarations
 variable "user_assigned_identities" {
   description = "User Assigned Identities (Managed Identities) provisioned with Station"
   type = map(object({
@@ -29,6 +30,7 @@ variable "user_assigned_identities" {
   default = {}
 }
 
+# tflint-ignore: terraform_unused_declarations
 variable "groups" {
   description = "Groups provisioned with Station"
   type = map(object({
@@ -38,6 +40,7 @@ variable "groups" {
   default = {}
 }
 
+# tflint-ignore: terraform_unused_declarations
 variable "applications" {
   description = "Applications provisioned with Station"
   type = map(object({
@@ -47,6 +50,7 @@ variable "applications" {
   default = {}
 }
 
+# tflint-ignore: terraform_unused_declarations
 variable "resource_groups" {
   description = "User specified resource groups provisioned by Station"
   type = map(object({
