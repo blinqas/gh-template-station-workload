f36d5f46a22ef5340874f058852148f78e721b1e
Adrian <adrian.e.phillips00@gmail.com>
2025-11-26 13:00:01 +0100

    fix: remove unused variable environment_name

--- variables.tf
+++ variables.tf
@@ -3,11 +3,6 @@ variable "station_id" {
   description = "The id of the workload deployment"
 }
 
-variable "environment_name" {
-  type        = string
-  description = "The name of the workload deployment"
-}
-
 variable "workload_resource_group_name" {
   type        = string
   description = "The name of the resource group for the workload deployment"
